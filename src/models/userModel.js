9a3e039d6b3f753ebe26f7ad92d35f0665d90404
afzalimamias <afzalimamias@gmail.com>
2022-12-18 23:49:25 +0530

    added profile image field

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -55,6 +55,11 @@ const userSchema = new Schema({
         enum: ["student", "faculty"],
         required: [true, "Role is required!"],
     },
+    profileImg: {
+        type: String,
+        default:
+            "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+    },
     isAdmin: {
         type: Boolean,
         default: false,

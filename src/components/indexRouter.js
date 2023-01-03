545ad69b7a73b583bedea85effd6a883390eb9b6
afzalimamias <afzalimamias@gmail.com>
2023-01-03 18:35:01 +0530

    added sendgrid to send email

--- src/components/indexRouter.js
+++ src/components/indexRouter.js
@@ -4,12 +4,14 @@ import noticeRoute from "./notice/noticeRoute.js";
 import notificationRoute from "./notification/notificationRoute.js";
 import authRoute from "./auth/authRoute.js";
 import userRoute from "./users/userRoute.js";
+import emailRoute from "./email/emailRoute.js";
 
 const router = Router();
 
 router.use("/auth", authRoute);
 router.use("/user", userRoute);
 router.use("/notice", noticeRoute);
+router.use("/email", emailRoute);
 router.use("/notification", notificationRoute);
 
 export default router;
--- src/core/config.js
+++ src/core/config.js
@@ -10,6 +10,9 @@ export default {
             useUnifiedTopology: true,
         },
     },
+    sendgrid: {
+        api_key: process.env.SENDGRID_API_KEY,
+    },
     cloudinary: {
         cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
         api_key: process.env.CLOUDINARY_API_NAME,
